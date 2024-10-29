0dd2b18b3260c1e8b0d439a91c7e6e7763d7dfb8
kcfan041 <davidfan1104@gmail.com>
2024-10-29 17:24:41 +0800

    更新 上傳完成後移動

--- api.py
+++ api.py
@@ -1,29 +1,35 @@
 import requests
+import shutil
 import time
 import json
+import copy
 import os
 import re
 
 
 
 def main():
-        # 遍歷 output/patient 資料夾中的每個檔案並上傳
+    
+    check_and_build()
+    
     patient_dir = "output/patient"
     for filename in os.listdir(patient_dir):
         file_path = os.path.join(patient_dir, filename)
         with open(file_path, 'r', encoding='utf-8') as file:
             patient_data = json.load(file)
-            patient_id = patient_data["id"]  # 假設檔案名稱是患者 ID
-            Patient_put(patient_id, patient_data)
+            patient_id = patient_data["id"]  
+            tf = Patient_put(patient_id, patient_data)
+        if tf:
+            shutil.move(f"output/Patient/{filename}", f"output/success/Patient/{filename}")
 
-    # 遍歷 output/BC 資料夾中的每個檔案並上傳
     observation_dir = "output/BC"
     for filename in os.listdir(observation_dir):
-        file_path = os.path.join(observation_dir, filename)
+        file_path = os.path.join(observation_dir)
         with open(file_path, 'r', encoding='utf-8') as file:
             observation_data = json.load(file)
-            Observation_put(observation_data)
-
+            tf = Observation_put(observation_data, filename)
+        if tf:
+            shutil.move(f"output/BC/{filename}", f"output/success/BC/{filename}")
 
 
 base_url = "https://kcfhir.dicom.tw/fhir"
@@ -46,12 +52,14 @@ def Patient_put(id, patient_data):
             print(f'Patient {patient_data["id"]} updated successfully.')
             print("Status Code:", response.status_code)
             print("Response:", response.json())
+            return True
         else:
             print("Failed to update Patient.")
             print("Status Code:", response.status_code)
             print("Response:", response.json())
+            return False
     except :
-        pass
+        return False
 
 def Observation_put(observation_data):
     time.sleep(0.3)
@@ -62,20 +70,31 @@ def Observation_put(observation_data):
         "Content-Type": "application/fhir+json"
     }
     response = requests.post(url, headers=headers, data=json.dumps(observation_data))
-
-
-    if response.status_code == 201:
-        subject_reference = observation_data["subject"]["reference"]
-        patient_id = re.search(r"Patient/(.*)", subject_reference).group(1)
-        time = observation_data["effectiveDateTime"]
-        name = observation_data["code"]["coding"][0]["display"].replace(" ", "_")
-        total = patient_id + time + name
-        print(f"Observation {total} updated successfully.")
-        print("Status Code:", response.status_code)
-        print("Response:", response.json())
-    else:
-        print("Failed to update Observation.")
-        print("Status Code:", response.status_code)
-        print("Response:", response.json())
+    try:
+        if response.status_code == 201:
+            od = copy.deepcopy(observation_data)
+            subject_reference = od["subject"]["reference"]
+            patient_id = re.search(r"Patient/(.*)", subject_reference).group(1)
+            dtime = od["effectiveDateTime"]
+            name = od["code"]["coding"][0]["display"].replace(" ", "_")
+            total = patient_id + dtime + name
+            print(f"Observation {total} updated successfully.")
+            print("Status Code:", response.status_code)
+            print("Response:", response.json())
+            return True
+        else:
+            print("Failed to update Observation.")
+            print("Status Code:", response.status_code)
+            print("Response:", response.json())
+            return False
+    except :
+        return False
         
+def check_and_build():
+    if not os.path.exists("./output/success"):
+        os.makedirs("./output/success")
+    if not os.path.exists("./output/success/BC"):
+        os.makedirs("./output/success/BC")
+    if not os.path.exists("./output/success/Patient"):
+        os.makedirs("./output/success/Patient")
 main()
